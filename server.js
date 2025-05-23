25394a41b7ce2a9b36932cc5d01ec15138eda93a
Govind Kumar <govind.kumar@nzminds.com>
2025-05-23 12:06:57 +0530

    Resolve CORS issue

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:"https://attendance-tracker-ruby-alpha.vercel.app"
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/attendance", attendanceRoutes);
