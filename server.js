b1d00d77e9381f23eafbfee7e37c695e3795a698
Govind Kumar <govind.kumar@nzminds.com>
2025-05-23 13:51:00 +0530

    Resolve domain origin

--- server.js
+++ server.js
@@ -5,14 +5,27 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const attendanceRoutes = require("./routes/attendanceRoutes");
 
+const allowedOrigins = [
+  "https://attendance-tracker-19w6wv4jq-govind-kumars-projects-d18fa27b.vercel.app",
+  "https://attendance-tracker-mbraydf0b-govind-kumars-projects-d18fa27b.vercel.app",
+];
+
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({
-    origin:"https://attendance-tracker-19w6wv4jq-govind-kumars-projects-d18fa27b.vercel.app",
-    credentials:true
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("cors not allowed for this origin"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/attendance", attendanceRoutes);
